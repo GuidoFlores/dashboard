2c72ba9da5e366cbfbb842ca59c8fb21be499520
GuidoFlores <gjflores@espol.edu.ec>
2024-07-11 17:53:28 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,9 @@ function App() {
 
   {/* Variable de estado y función de actualización */ }
 
-  let [indicators, setIndicators] = useState<JSX.Element[]>([])
+  let [indicators, setIndicators] = useState([])
+  let [rowsTable, setRowsTable] = useState([])
+  let [chart, setChart] = useState([])
 
   {/* Hook: useEffect */ }
 
@@ -23,11 +25,48 @@ function App() {
 
     (async () => {
 
-      {/* Request */ }
+      {/* 1. Comente el código anterior con el Request */ }
 
-      let API_KEY = "b219edf6dd38676f8fcbfb53deb5e83e"
-      let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Guayaquil&mode=xml&appid=${API_KEY}`)
-      let savedTextXML = await response.text();
+      // {/* Request */ }
+
+      // let API_KEY = "AQUÍ VA SU API KEY DE OPENWEATHERMAP"
+      // let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Guayaquil&mode=xml&appid=${API_KEY}`)
+      // let savedTextXML = await response.text();
+
+
+      {/* 2. Del LocalStorage, obtiene el valor de las claves openWeatherMap y expiringTime */ }
+
+      let savedTextXML = localStorage.getItem("openWeatherMap")
+      let expiringTime = localStorage.getItem("expiringTime")
+
+      {/* 3. Obtenga la estampa de tiempo actual */ }
+
+      let nowTime = (new Date()).getTime();
+
+      {/* 4. Realiza la petición asicrónica cuando: 
+                 (1) La estampa de tiempo de expiración (expiringTime) es nula, o  
+                 (2) La estampa de tiempo actual es mayor al tiempo de expiración */}
+
+      if (expiringTime === null || nowTime > parseInt(expiringTime)) {
+
+        {/* 5. Request */ }
+
+        let API_KEY = "b219edf6dd38676f8fcbfb53deb5e83e"
+        let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Guayaquil&mode=xml&appid=${API_KEY}`)
+        savedTextXML = await response.text();
+
+
+        {/* 6. Diferencia de tiempo */ }
+
+        let hours = 1
+        let delay = hours * 3600000
+
+
+        {/* 7. En el LocalStorage, almacena texto en la clave openWeatherMap y la estampa de tiempo de expiración */ }
+
+        localStorage.setItem("openWeatherMap", savedTextXML)
+        localStorage.setItem("expiringTime", (nowTime + delay).toString())
+      }
 
       {/* XML Parser */ }
 
@@ -62,7 +101,21 @@ function App() {
         (element) => <Indicator title={element[0]} subtitle={element[1]} value={element[2]} />
       )
 
+      let arrayObjects = Array.from(xml.getElementsByTagName("time")).map((timeElement) => {
+
+        let rangeHours = timeElement.getAttribute("from").split("T")[1] + " - " + timeElement.getAttribute("to").split("T")[1]
+
+        let windDirection = timeElement.getElementsByTagName("windDirection")[0].getAttribute("deg") + " " + timeElement.getElementsByTagName("windDirection")[0].getAttribute("code")
+
+        return { "rangeHours": rangeHours, "windDirection": windDirection }
+
+      })
+
+      arrayObjects = arrayObjects.slice(0, 8)
+
+      setRowsTable(arrayObjects)
       setIndicators(indicatorsElements)
+
     })()
 
   }, [])
@@ -71,36 +124,33 @@ function App() {
 
   return (
     <Grid container spacing={5}>
-      <Grid xs={6} lg={2}>
 
+      <Grid xs={6} lg={2}>
         {indicators[0]}
-
-        {/* <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /> */}
-
       </Grid>
+      
       <Grid xs={6} lg={2}>
-
         {indicators[1]}
-
-        {/* <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /> */}
-
       </Grid>
+
       <Grid xs={6} md={4} lg={2}>
         {indicators[2]}
-
-        {/* <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /> */}
       </Grid>
-      <Grid xs={6} sm={4} md={3} lg={2}>
+
+      <Grid xs={6} sm={4} md={3} lg={6}>
         <Summary></Summary>
       </Grid>
+
       <Grid xs={12} lg={2}>
         <ControlPanel />
       </Grid>
+
       <Grid xs={12} lg={10}>
         <WeatherChart></WeatherChart>
       </Grid>
-      <Grid xs={12} md={6} lg={9} >
-        <BasicTable />
+
+      <Grid xs={12} lg={12}>
+        <BasicTable rows={rowsTable}></BasicTable>
       </Grid>
     </Grid>
 
