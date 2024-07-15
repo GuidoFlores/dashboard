5a53795378444dfbf4e19a65b154ed34aab7e081
GuidoFlores <gjflores@espol.edu.ec>
2024-07-15 12:20:57 -0500

    Agregando diseño

--- src/components/Indicator.tsx
+++ src/components/Indicator.tsx
@@ -12,21 +12,21 @@ export default function Indicator(config: Config) {
     return (
         <Paper
         sx={{
-          p: 2,
           display: 'flex',
-          flexDirection: 'column'
+          flexDirection: 'column',
         }}
       >
-        <Typography component="h2" variant="h6" color="primary" gutterBottom>
+        <Typography component="h2" variant="h6" color="primary" 
+        sx={{ color: 'blue', backgroundColor: '#a1d9e2', p: 1, borderRadius: 1 }}>
             {config.title} 
         </Typography>
-        <Typography color="text.secondary" sx={{ flex: 1 }}>
+        <Typography color="text.secondary" sx={{ flex: 1 , color: '#000000', backgroundColor: '#f8f7f8', p: 1, borderRadius: 1  }}>
             {config.subtitle}
         </Typography>
-        <Typography color="text.secondary" sx={{ flex: 1 }}>
+        <Typography color="text.secondary" sx={{ flex: 1, color: '#000000', backgroundColor: '#f8f7f8', p: 1, borderRadius: 1}}>
             {config.value.toString()}
         </Typography>
-        <Typography color="text.secondary" sx={{ flex: 1 }}>
+        <Typography color="text.secondary" sx={{ flex: 1, color: '#000000', backgroundColor: '#f8f7f8', p: 1, borderRadius: 1}}>
             {config.value2.toString()}
         </Typography>
     </Paper> 
